60b92f49e2100c44887c294f31f0798497d35450
GEHgio1012 <giovannihernandez1012@gmail.com>
2022-03-19 23:02:05 -0600

    Agregando slider no terminado

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
     include "encabezado.php";
 ?>
+
+<div class="slider">
+    <div><h1>hola</h1></div>
+    <ul>
+        <li><img src="recursos/slider1.jpg" alt=""></li>
+        <li><img src="recursos/slider2.jpg" alt=""></li>
+        <li><img src="recursos/slider3.jpg" alt=""></li>
+        <li><img src="recursos/slider4.jpg" alt=""></li>
+    </ul>
+</div>
